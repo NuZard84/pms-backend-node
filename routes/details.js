d1421157a40d07e05dd6ec5aa82035e53133d637
vaibhav0712 <saneparavaibhav73@gmail.com>
2024-02-29 23:58:38 +0530

    updated 29 feb 12 PM

--- routes/details.js
+++ routes/details.js
@@ -2,24 +2,6 @@ const router = require("express").Router();
 const Paitent = require("../models/patientModal");
 const Doctor = require("../models/doctorModal");
 
-// router.post("/patient", async (req, res) => {
-//   const { email, gender, age, phoneNumber } = req.body;
-//   console.log(req.body);
-//   try {
-//     const patient = await Paitent.findOne({ email });
-//     patient.gender = gender;
-//     patient.age = age;
-//     patient.phoneNumber = phoneNumber;
-//     patient.isDetailsFilled = true;
-
-//     await patient.save();
-//     return res.status(201).json(patient);
-//   } catch (error) {
-//     return res.status(404).json({
-//       message: "Error saving details",
-//     });
-//   }
-// });
 router.post("/doctor", async (req, res) => {
   const { email, name, gender, age, phoneNumber, category, education } =
     req.body;
@@ -39,6 +21,55 @@ router.post("/doctor", async (req, res) => {
     console.log("doctor full details saved into database!");
     return res.status(201).json(doctor);
   } catch (error) {
+    return res.status(404).json({
+      message: "Error saving details",
+    });
+  }
+});
+
+router.post("/update/doctor", async (req, res) => {
+  const { email, name, gender, age, phoneNumber, category, education } =
+    req.body;
+  console.log(req.body);
+  try {
+    const doctor = await Doctor.findOne({ email });
+    const updateDoctor = {
+      ...doctor,
+      specialist: category || doctor.specialist,
+      name: name || doctor.name,
+      gender: gender || doctor.gender,
+      age: age || doctor.age,
+      phoneNumber: phoneNumber || doctor.phoneNumber,
+      education: education || doctor.education,
+    };
+    Object.assign(doctor, updateDoctor);
+    await doctor.save();
+    res.status(201).json(doctor);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({
+      message: "Error saving details",
+    });
+  }
+});
+
+router.post("/update/patient", async (req, res) => {
+  const { email, name, gender, age, phoneNumber } = req.body;
+  console.log(req.body);
+  try {
+    const patient = await Paitent.findOne({ email });
+    const updatePatient = {
+      ...patient,
+      name: name || patient.name,
+      gender: gender || patient.gender,
+      age: age || patient.age,
+      phoneNumber: phoneNumber || patient.phoneNumber,
+    };
+    Object.assign(patient, updatePatient);
+    await patient.save();
+    res.status(201).json(patient);
+  } catch (err) {
+    console.log(err);
     return res.status(404).json({
       message: "Error saving details",
     });
